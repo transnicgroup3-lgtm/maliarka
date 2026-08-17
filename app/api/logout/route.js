ddbb502b53f292a0f47597b75096b86d1012a121
transnicgroup3-lgtm <transnicgroup3@gmail.com>
2026-08-17 13:38:28 +0300

    Delete app/api/logout directory

--- app/api/logout/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { AUTH_COOKIE_NAME } from "../../../lib/auth";
-
-export async function POST() {
-  const response = NextResponse.json({ ok: true });
-  response.cookies.delete(AUTH_COOKIE_NAME);
-  return response;
-}
