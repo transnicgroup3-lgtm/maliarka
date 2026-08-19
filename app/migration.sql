1b441b02631a108c4737e623cc3e01bf22e9f9e3
transnicgroup3-lgtm <transnicgroup3@gmail.com>
2026-08-19 11:08:11 +0300

    Delete app/migration.sql

--- app/migration.sql
+++
@@ -1,5 +0,0 @@
--- Ruleaza acest fisier in Supabase SQL Editor pe proiectul TAU EXISTENT
--- (adauga doar coloanele noi, nu sterge nimic din datele deja introduse)
-
-alter table masini add column if not exists model text;
-alter table masini_materiale add column if not exists cost numeric;
